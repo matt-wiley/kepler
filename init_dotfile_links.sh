48f6ad336fa2e1fb3816f4e2d49bac5330fa45a5
Matt Wiley <mattwiley+github@fastmail.com>
2025-04-22 17:42:11 -0400

    added: felt semver-y, might delete later

--- init_dotfile_links.sh
+++ init_dotfile_links.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
 
-
+# This script creates symbolic links for dotfiles in the user's home directory.
+# 
+# It can be rerun as needed to pick up new dotfiles. Changes to existing 
+# dotfiles that have already been linked will not be overwritten.
+#
+# It uses the KEPLER_HOME environment variable to determine the source of the 
+# dotfiles.
+# 
+# If KEPLER_HOME is not set, it runs in dry run mode, printing the commands 
+# instead of executing them.
 
 update_dotfile_links() {
     local kepler_home="${KEPLER_HOME:-"KEPLER_HOME"}"
-    
+
     local link_map=(
         ".vimrc|lib/vimrc/amix-basic.vimrc"
     )
