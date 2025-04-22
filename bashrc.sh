43b0a4ea73b4551722b13c8dd966bc6430976ade
Matt Wiley <mattwiley+github@fastmail.com>
2025-04-22 17:53:50 -0400

    added: core aliases

--- bashrc.sh
+++ bashrc.sh
@@ -1,2 +1,5 @@
 # Source specific bashrc file based on the system architecture
 source "${KEPLER_HOME}/lib/uname/$(uname)/bashrc.sh" || true
+
+
+source "${KEPLER_HOME}/lib/aliases/core.sh" || true
---
+++ lib/aliases/core.sh
@@ -0,0 +1,7 @@
+## Sorted Environment Variables
+alias envs='env | sort'
+
+# List files
+alias ls='ls --color=always' # Always show colors
+alias ll='ls -lGh' # Long list 
+alias lll='ls -laGh' # Long list all 
